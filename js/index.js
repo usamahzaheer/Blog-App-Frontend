f46b1b2bfe92bf4c496a9d1d1a529cae939a6d02
usamahzaheer <sardarosamahkhan@gmail.com>
2021-02-17 01:25:01 +0500

    done with the remained part..last commit

--- js/index.js
+++ js/index.js
@@ -22,21 +22,21 @@ const buildPosts = (blogPosts) => {
         const postLink = `/post.html?id=${blogPost.id}`;
         const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
         blogPostsContent += `
-            <a class="post-link" href="${postLink}">
-                <div class="post">
-                    <div class="post-image" style="background-image: url(${postImage})"></div>
-                    <div class="post-content">
-                        <div class="post-date">${postDate}</div>
-                        <div class="post-title">
-                            <h4>${blogPost.title}</h4>
-                        </div>
-                        <div class="post-text">
-                            <p>${blogPost.content}</p>
-                        </div>
+        <a class="post-link" href="${postLink}">
+            <div class="post">
+                <div class="post-image" style="background-image: url(${postImage})"></div>
+                <div class="post-content">
+                    <div class="post-date">${postDate}</div>
+                    <div class="post-title">
+                        <h4>${blogPost.title}</h4>
+                    </div>
+                    <div class="post-text">
+                        <p>${blogPost.content}</p>
                     </div>
                 </div>
-            </a>
-        `
+            </div>
+        </a>
+    `
     }
     document.querySelector(".blog-posts").innerHTML = blogPostsContent;
 }
